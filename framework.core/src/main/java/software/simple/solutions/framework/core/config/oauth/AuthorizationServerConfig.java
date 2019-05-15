abfb8280d7a9f8517becf22e7af811f839cb76d7
yusuf nazir <yusuf.nazir@gmail.com>
2019-05-15 20:48:03 -0300

    switched to jdbctokenstore

--- framework.core/src/main/java/software/simple/solutions/framework/core/config/oauth/AuthorizationServerConfig.java
+++ framework.core/src/main/java/software/simple/solutions/framework/core/config/oauth/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package software.simple.solutions.framework.core.config.oauth;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +14,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
@@ -21,6 +23,9 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	private final AuthenticationManager authenticationManager;
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
@@ -57,6 +62,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Bean
 	TokenStore tokenStore() {
-		return new JwtTokenStore(this.jwtAccessTokenConverter());
+		// return new JwtTokenStore(this.jwtAccessTokenConverter());
+		return new JdbcTokenStore(dataSource);
 	}
 }
