b5d99bdb1e63a7dc701dd5086ec6a790995901f6
yusuf nazir <yusuf.nazir@gmail.com>
2019-05-26 20:59:41 -0300

    show creating new item message in actionbar

--- framework.core.vaadin8/framework.core.vaadin8.web/src/main/java/software/simple/solutions/framework/core/web/BasicTemplate.java
+++ framework.core.vaadin8/framework.core.vaadin8.web/src/main/java/software/simple/solutions/framework/core/web/BasicTemplate.java
@@ -382,6 +382,7 @@ public abstract class BasicTemplate<T> extends AbstractBaseView implements GridT
 		actionBar.setBackDisabled();
 		actionBar.setInfoDisabled();
 		actionBar.setAuditDisabled();
+		actionBar.hideCreatingItem();
 
 		actionBar.authorizeNew();
 		actionBar.authorizeSearch();
@@ -1233,6 +1234,7 @@ public abstract class BasicTemplate<T> extends AbstractBaseView implements GridT
 		actionBar.setSearchDisabled();
 		actionBar.authorizeSave();
 		actionBar.authorizeBack();
+		actionBar.authorizeCreatingItem();
 
 		if (entity != null) {
 			actionBar.authorizeDelete();
